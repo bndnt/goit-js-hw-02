5d6838452da15ad34f25f94e81d69cbbfe7ef043
bndnt <67003594+bndnt@users.noreply.github.com>
2024-03-06 16:28:22 -0500

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,13 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    let totalPrice = 0;
-    if(customerCredits < pricePerDroid*quantity){
-        return ('Insufficient funds!');
-    }
-    else{
-        totalPrice = quantity*pricePerDroid;
-        return (`You ordered ${quantity} droids worth ${totalPrice} credits!`)
-    }
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  let totalPrice = 0;
+  if (customerCredits < pricePerDroid * quantity) {
+    return "Insufficient funds!";
+  } else {
+    totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
-console.log('task-1'); 
+console.log("task-1");
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,12 @@
-function formatMessage(message, maxLength){
-    if (maxLength>=message.length){
-        return message;
-    }
-    else{
-        strNew=message.slice(0,maxLength);
-        return (strNew + "...");
-    }
+function formatMessage(message, maxLength) {
+  if (maxLength >= message.length) {
+    return message;
+  } else {
+    strNew = message.slice(0, maxLength);
+    return strNew + "...";
+  }
 }
-console.log('task-2'); 
+console.log("task-2");
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
